deed5a4c144261e45f44a95fe62b20f741aa408f
josegazano732 <josegazano7@outlook.com>
2022-10-07 16:03:45 -0300

    3-ejercicios-basicos. JLG

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
 
 
 
+function sumando(a:number,b:number):number{
+    return a + b;
+}
+
+const sumarFlech = (a:number, b:number):number => {
+    return a + b;
+}
+
+const resultado = sumando(10,20);
+
+console.log(resultado);
 
 
 
